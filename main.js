e0e5c265d8c99d65dd34aba5bf722eabfe238e11
Ebrahim Shamskilani <e.shams@tci.ir>
2022-08-30 18:59:16 +0430

    adding table is valid function and split function to convert 1D function to 2D function

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function rotateTable(input_table_array)
 {
-   return table_is_valid(input_table_array)
+   return split_rows(input_table_array)
 }
 
 function table_is_valid(input_table_array)
 {
+    // this function check if the array is squre or not
     if (Number.isInteger(Math.sqrt(input_table_array.length))) 
     {
         return true;
@@ -15,4 +16,29 @@ function table_is_valid(input_table_array)
     }
 }
 
-console.log(rotateTable([1,2,3,4]))
+function split_rows(input_table_array) 
+{
+   // this function convert a 1D array to a 2D array
+   // return an array of rows 
+   var splited_rows=[];
+   var temp_row=[];
+   var edge_counter=0;
+   var input_array_length=input_table_array.length;
+   var edge=Math.sqrt(input_array_length); // number of column and rows
+
+   for(i=0;i<input_array_length;i++)
+   {
+      temp_row.push(input_table_array[i]);
+      edge_counter++;
+      if(edge_counter==edge)
+      {
+        edge_counter=0;
+        splited_rows.push(temp_row);
+        temp_row=[];
+      }
+
+   }
+   return splited_rows
+}
+
+console.log(rotateTable([1,2,3,4,5,6,'j',8,9,10,11,12,13,14,15,16]))
